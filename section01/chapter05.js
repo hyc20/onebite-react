1bf24a58601d05162b4bb3549010fd6973e576b7
hyc20 <hyc104389@gmail.com>
2025-12-03 11:12:46 +0900

    원시타입

---
+++ section01/chapter05.js
@@ -0,0 +1,45 @@
+//원시타입
+//1. number type
+let num1 = 28;
+let num2 = 1.5;
+let num3 = -20;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+let inf = Infinity;
+let mInf = -Infinity;
+let nan = NaN; //Not a Number
+// console.log(1 * "hello");//NaN
+
+//2. String Type
+let myName = "이정황"; 
+let myLocation = "목동";
+let introduce = myLocation + myName;
+// console.log(introduce);
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+// console.log(introduceText);
+
+//3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+//4. Null Type 아무것도 없다
+let empty = null;//명시적으로 null이라는 값을 할당해야 들어간다
+
+//5. undefined Type
+let none;//아무것도 할당하지 않았을 때 자동으로 들어가는 값
+console.log(none)
+
+
+
+
+
+
+
+
+
