91ceed8ff25753804c1f48394fc05acddc562e62
hyc20 <hyc104389@gmail.com>
2025-12-03 12:47:32 +0900

    연산자까지

---
+++ section01/chapter08.js
@@ -0,0 +1,44 @@
+//1. null 병합 연산자
+//존재하는 값을 추려내는 기능
+//null, undefined가 아닌 값을 찾아내는 연산자 
+
+let var1;
+let var2 = 10;
+let var3 = 20;
+
+let var4 = var1 ?? var2;
+console.log(var4)
+
+let var5 = var1 ?? var3;
+console.log(var5)
+
+let var6 = var2 ?? var3; 
+console.log(var6);
+
+let userName = "홍용천";
+let userNickName = "hyc";
+//userName이 null이나 undefined가 아니면 displayName에 저장하고 
+//null이나 undefined이면 userNickName을 displayName에 저장
+let displayName = userName ?? userNickName;
+console.log(displayName)
+
+//2. typeof 연산자
+//값의 타입을 문자로 반환하는 연산자 
+let var7 = "hello";
+let t1 = typeof var7;
+console.log(t1);
+
+//3. 삼항 연산자
+//항 3개를 사용하는 연산자
+//조건 식을 이용해 참, 거짓일 때의 값을 다르게 반환
+//요구사항 : 변수 res에 var8의 값이 짝수 -> "짝", 홀수 -> "홀"
+let var8 = 10;
+let res = var8%2===0 ? "짝" : "홀";
+console.log(res);
+
+
+
+
+
+
+
