0279ac9708a888ab133b8cb61536383a34b8d73c
hyc20 <hyc104389@gmail.com>
2025-12-03 10:53:43 +0900

    리액트 시작 전 자바스크립트

---
+++ index.js
@@ -0,0 +1,13 @@
+//1 변수
+let age = 27; //선언과 동시에 초기화
+console.log(age);
+age = 24; //재할당 
+console.log(age);
+
+//let age =30; //블록 범위의 같은 변수는 재선언 불가능
+
+
+//상수
+const birth = '2020.11.12'; //상수는 선언과 동시에 초기화해야함
+// birth = '2020.11.13'; 상수는 재할당 불가능
+console.log(birth)
