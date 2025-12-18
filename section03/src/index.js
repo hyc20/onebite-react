4ffce41425b76c04cb20069cbfba396969ab08e2
hyc20 <hyc104389@gmail.com>
2025-12-18 21:06:20 +0900

    node 맛보기

---
+++ section03/src/index.js
@@ -0,0 +1,21 @@
+// console.log("안녕 node.js");
+
+//common js 모듈 방식
+// const mathModule = require('./math');
+// const {add, sub} = require('./math');
+// console.log(mathModule);
+// console.log(mathModule.add(1,2));
+// console.log(mathModule.sub(1,2));
+
+// console.log(add(1,2))
+// console.log(sub(1,2))
+
+
+// import {add,sub} from './math.js';
+// console.log(add(1,2))
+// console.log(sub(1,2))
+
+import randomColor from "randomcolor";
+const color = randomColor();
+console.log(color);
+
---
+++ section03/src/math.js
@@ -0,0 +1,19 @@
+//math 모듈
+function add(a,b){
+  return a+b;
+}
+
+function sub(a,b){
+  return a-b;
+}
+
+//common js 시스템 
+// module.exports = {
+//   add,
+//   sub
+// }
+
+//ESM 
+export {
+  add, sub
+}
